53f6130ee0f417668a1bcaa8ba3ce78b3d0e8f49
CaroleLavillette <pitouDev@users.noreply.github.com>
2019-09-03 16:23:06 +0200

    add axios

--- src/App.js
+++ src/App.js
@@ -3,15 +3,37 @@ import './App.css';
 import Users from "./containers/Users";
 import Expenses from "./containers/Expenses";
 import NewExpense from "./containers/NewExpense";
+import axios from "axios";
 
-function App() {
-  return (
+export default class App extends React.Component {
+  state={
+    data:""
+  }
+  
+  render() {
+    return (
     <div className="App">
+      <div>
       <Users></Users>
+      </div>
+      <div>
       <Expenses></Expenses>
+      </div>
+      <div>
       <NewExpense></NewExpense>
+      </div>
     </div>
   );
-}
+  }
+  async componentDidMount() {
+  try {
+      const response = await axios.get("https://cl-expenses-back.herokuapp.com/");
+      this.setState({data: response.data})
+     console.log(response.data)
+  } catch (error) { this.setState({ error: true})
 
-export default App;
+  }
+}}
+
+
+
--- src/components/form.js
+++ src/components/form.js
@@ -3,22 +3,32 @@ import React from "react";
 export default class Form extends React.Component {
     render() {
         return(
-            <form>
+            <form style={{display:"flex", flexDirection:"column"}}>
                 <input 
+                    className="input"
                     type="text" 
                     name="user" 
                     placeholder="User"
                     minLength="15"/>
                 <input 
+                    className="input"
                     type="text" 
                     name="description" 
                     placeholder="Description"
                     minLength="25"/>
                 <input 
+                    className="input"
                     type="number" 
                     name="amount" 
                     placeholder="Amount"
                     />
+                <input
+                    className="button"
+                    type="submit"
+                    name="addExpense"
+                    value="ADD EXPENSE"
+                    />
+
             </form>
         )
     }
